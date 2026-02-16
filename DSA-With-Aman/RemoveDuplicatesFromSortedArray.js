802edbdaff894ce58808ad93a6bbaa84b1c25e33
amanuniyal01 <amanuniyal315@gmail.com>
2026-02-16 12:24:00 +0530

    Handling non increasing case also

--- DSA-With-Aman/RemoveDuplicatesFromSortedArray.js
+++ DSA-With-Aman/RemoveDuplicatesFromSortedArray.js
@@ -3,15 +3,18 @@
 
 const RemoveDuplicates = (arr) => {
     let x = 0;
-    for (let i = 1; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
+        // It is for non decreasing for non increasing (arr[x]>arr[i])
         if (arr[x] < arr[i]) {
+
             x = x + 1
             arr[x] = arr[i]
         }
     }
     return x + 1
     return arr[x]
-    
+
 }
 
 console.log(RemoveDuplicates([1, 2, 2, 3, 3, 4, 4, 5]))
+// console.log(RemoveDuplicates([5,4,4,3,3,2,2,1]))
