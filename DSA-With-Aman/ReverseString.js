c06a9cd8819991e3fcaac6752fdbc53aa4e995dd
amanuniyal01 <amanuniyal315@gmail.com>
2026-02-17 11:42:34 +0530

    Day 04 - Practice all types of reversing string problems✅✅

--- DSA-With-Aman/ReverseString.js
+++ DSA-With-Aman/ReverseString.js
@@ -9,8 +9,25 @@ const ReverseString = (s) => {
     }
 };
 
-let arr = ["h","e","l","l","o"];
+let arr = ["h", "e", "l", "l", "o"];
 
 ReverseString(arr);
 
 console.log(arr);
+
+
+// Q2
+const reverseEachWord = (str) => {
+    return str
+        .split(" ")
+        .map(word => word.split("").reverse().join(""))
+        .join(" ");
+};
+
+console.log(reverseEachWord("I love Js")) // "I evol sJ"
+
+// Q 3
+const reverseWords = (str) => {
+    return str.split(" ").reverse().join(" ");
+};
+console.log(reverseWords("I love Js")) //Js love I
