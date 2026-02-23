b213039e08ed5d76953c250e1265e0d53ffc539e
amanuniyal01 <amanuniyal315@gmail.com>
2026-02-23 12:46:19 +0530

    Day 08- Solved SingleNumber Problem with both approaches

--- DSA-With-Aman/SIngleNumber.js
+++ DSA-With-Aman/SIngleNumber.js
@@ -1,21 +1,33 @@
-const SingleNumber = (num) => {
-    let hash = {};
+// 1st Approach..
+// const SingleNumber = (num) => {
+//     let hash = {};
 
-    // Build frequency map
-    for (let i = 0; i < num.length; i++) {
-        if (!hash[num[i]]) {
-            hash[num[i]] = 1;
-        } else {
-            hash[num[i]]++;
-        }
+//     // Build frequency map
+//     for (let i = 0; i < num.length; i++) {
+//         if (!hash[num[i]]) {
+//             hash[num[i]] = 1;
+//         } else {
+//             hash[num[i]]++;
+//         }
+//     }
+
+//     // Find element with frequency 1
+//     for (let i = 0; i < num.length; i++) {
+//         if (hash[num[i]] === 1) {
+//             return num[i];
+//         }
+//     }
+// };
+
+// console.log(SingleNumber([1, 1, 2, 2, 3, 3, 4, 5, 5]));
+
+const SingleNumber = (arr) => {
+    let xor = 0;
+    for (let i = 0; i < arr.length; i++) {
+        xor = xor ^ arr[i]
     }
+    return xor
 
-    // Find element with frequency 1
-    for (let i = 0; i < num.length; i++) {
-        if (hash[num[i]] === 1) {
-            return num[i];
-        }
-    }
-};
 
-console.log(SingleNumber([1, 1, 2, 2, 3, 3, 4, 5, 5]));
+}
+console.log(SingleNumber([1, 1, 2, 2, 3, 4, 4, 5, 5]))
