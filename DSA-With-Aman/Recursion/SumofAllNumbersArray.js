2b1b61ad1e9d065c40a65173588999dd91b4a413
amanuniyal01 <amanuniyal315@gmail.com>
2026-02-24 13:20:29 +0530

    Day 09- Solved Sum of All Numbers with all types in an Array using Recursion✅

--- DSA-With-Aman/Recursion/SumofAllNumbersArray.js
+++ DSA-With-Aman/Recursion/SumofAllNumbersArray.js
@@ -9,3 +9,21 @@ const SumofAllNumbers = (n) => {
 
 }
 console.log(SumofAllNumbers(arr.length - 1))
+
+
+// Sum of odd Numbers in an Array
+let arr2 = [1, 2, 3, 4, 5, 6]
+
+const OddSum = (n) => {
+    if (n < 0) return 0;
+
+    const isOdd = arr2[n] % 2 !== 0;
+
+    if (isOdd) {
+        return arr2[n] + OddSum(n - 1);
+    } else {
+        return OddSum(n - 1);
+    }
+}
+
+console.log(OddSum(arr2.length - 1));
