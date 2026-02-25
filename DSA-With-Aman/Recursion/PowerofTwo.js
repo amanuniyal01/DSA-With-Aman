41828062a97523672d8b657a42c134c11e09dd02
amanuniyal01 <amanuniyal315@gmail.com>
2026-02-25 15:18:42 +0530

    Day 10 - Solved PowerofTwo  Problem with Recursion  Method✅

--- DSA-With-Aman/Recursion/PowerofTwo.js
+++ DSA-With-Aman/Recursion/PowerofTwo.js
@@ -1,11 +1,23 @@
 // Normal Method
 const PowerofTwo = (num) => {
     if (num <= 0) return false;
-    while (num % 4 === 0) {
-        num = num / 4
+    while (num % 2 === 0) {
+        num = num / 2
     }
-    return num===1
+    return num === 1
 
 
 }
 console.log(PowerofTwo(16))
+
+// RECURSION
+const Poweroftwo = (n) => {
+    if (n === 1) return true;
+    else if (n < 1 || (n % 2 != 0)) {
+        return false;
+    }
+    return Poweroftwo(n / 2)
+
+
+}
+console.log(Poweroftwo(26))
