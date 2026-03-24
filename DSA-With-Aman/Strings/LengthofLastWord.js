017018b1dfa3ae37f1547b3e19762601120caeae
amanuniyal01 <amanuniyal315@gmail.com>
2026-03-24 10:31:30 +0530

    Day 19- Solved Length of Last word Problem using 2nd Approach

--- DSA-With-Aman/Strings/LengthofLastWord.js
+++ DSA-With-Aman/Strings/LengthofLastWord.js
@@ -1,8 +1,26 @@
 // a="Hello world" Output=5 bcz world is last word
-const LengthofLastWord = (str) => {
-    let s = str.trim()
-   s= s.split(" ")
-    return s[s.length - 1].length
+// Using Built in Functions(Not recommended for interviews.)
+// const LengthofLastWord = (str) => {
+//     let s = str.trim()
+//    s= s.split(" ")
+//     return s[s.length - 1].length
 
+// }
+// console.log(LengthofLastWord("aman Uniyal zoo    "))
+
+
+// Approach -2 (Using Loops)
+const LengthofLastWord = (str) => {
+    let word = str.trim();
+    let count = 0;
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        if (word[i] === " ") {
+            break;
+        }
+        count++;
+    }
+
+    return count;
 }
-console.log(LengthofLastWord("aman Uniyal zoo    "))
+console.log(LengthofLastWord("      aman uniyal is a good boy"))
