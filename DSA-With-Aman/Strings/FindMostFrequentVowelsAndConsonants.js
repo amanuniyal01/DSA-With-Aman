2dfd82c34e5c66ef81097bec03fb00e06e8d8243
amanuniyal01 <amanuniyal315@gmail.com>
2026-03-25 17:41:38 +0530

    Day 20 - Solved Find Most Frequent Vowels and Consonants Problem✅

---
+++ DSA-With-Aman/Strings/FindMostFrequentVowelsAndConsonants.js
@@ -0,0 +1,21 @@
+const FindMostFrequentVowelsAndConsonants = (s) => {
+    let map = {};
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]] = !map[s[i]] ? 1 : map[s[i]] + 1;
+    }
+    let maxVowel = 0;
+    let maxConsonant = 0;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let mapKeys = Object.keys(map)
+    for (let i = 0; i < mapKeys.length; i++) {
+        if (vowels.includes(mapKeys[i])) {
+            maxVowel = Math.max(maxVowel, map[mapKeys[i]])
+        }
+        else {
+            maxConsonant = Math.max(maxConsonant, map[mapKeys[i]])
+        }
+    }
+    return maxVowel + maxConsonant;
+
+}
+console.log(FindMostFrequentVowelsAndConsonants("success"))
