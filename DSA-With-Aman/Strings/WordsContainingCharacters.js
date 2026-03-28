8399153f107be97e245e599227eaf0df70a00eb2
amanuniyal01 <amanuniyal315@gmail.com>
2026-03-28 11:32:03 +0530

    Day 22 - Solved WordsContainingCharacter Problem using Two Loops Approach✅

--- DSA-With-Aman/Strings/WordsContainingCharacters.js
+++ DSA-With-Aman/Strings/WordsContainingCharacters.js
@@ -1,13 +1,28 @@
 //  words = Input: ["leet","code"], x = "e"   Output: [0,1]
 
 // By using Built in method(includes)
-const WordsContainingCharacters = (arr,x) => { 
-    let res=[];
-    for(let i=0;i<arr.length;i++){
-        if(arr[i].includes(x)){
-            res.push(i)
+// const WordsContainingCharacters = (arr,x) => { 
+//     let res=[];
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i].includes(x)){
+//             res.push(i)
+//         }
+//     }
+//     return res;
+// }
+// console.log(WordsContainingCharacters(["leet","code"],"e"))
+
+// By using Two Loops..
+const WordsContainingCharacters = (arr, x) => {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] === x) {
+                res.push(i)
+                break;
+            }
         }
     }
     return res;
 }
-console.log(WordsContainingCharacters(["leet","code"],"e"))
+console.log(WordsContainingCharacters(["leet", "code"], "e"))
