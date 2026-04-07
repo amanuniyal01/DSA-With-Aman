9ac77e9b65be813b00190fefe528375993aaed6c
amanuniyal01 <amanuniyal315@gmail.com>
2026-04-07 10:53:56 +0530

    Day 26  Solved JewelsAndStones Problem using Brute Force(Using two loops)✅

--- DSA-With-Aman/Strings/JewelsandStones.js
+++ DSA-With-Aman/Strings/JewelsandStones.js
@@ -1,14 +1,32 @@
 //  Input: jewels = "aA", stones = "aAAbbbb"   Output: 3 //aAA is present in stones
 
 // Approach 1 - Brute Force approach using includes.
-const JewelsandStones=(jewels,stones)=>{
-    let count=0;
-    for(let i=0;i<stones.length;i++){
-        if(jewels.includes(stones[i])){
-            count++;
+// const JewelsandStones=(jewels,stones)=>{
+//     let count=0;
+//     for(let i=0;i<stones.length;i++){
+//         if(jewels.includes(stones[i])){
+//             count++;
+//         }
+//     }
+//     return count
+
+// }
+// console.log(JewelsandStones("aA","aAAbbbb"))
+
+
+
+// Approach 2 - Brute force without using includes.
+const JewelsandStones = (jewels, stones) => {
+    let count = 0;
+    for (let i = 0; i < stones.length; i++) {
+        for (let j = 0; j < jewels.length; j++) {
+            if (jewels[j] === stones[i]) {
+                count++;
+                break;
+            }
         }
     }
     return count
 
 }
-console.log(JewelsandStones("aA","aAAbbbb"))
+console.log(JewelsandStones("aA", "aAAbbbb"))
