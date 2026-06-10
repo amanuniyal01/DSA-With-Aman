99d0b33c98e4ba1b6729aa10a6d32a4038d23514
amanuniyal01 <amanuniyal315@gmail.com>
2026-06-10 07:27:00 +0530

    Solved Count Digit Problem

--- CountDigit.js
+++ CountDigit.js
@@ -3,7 +3,7 @@ let count = 0;
 const CountDigit = (num) => {
     if (num === 0) return 1
     if (num < 0) {
-        num= num * (-1)
+        num = num * (-1)
     }
     while (num > 0) {
         num = Math.floor(num / 10);
@@ -13,4 +13,6 @@ const CountDigit = (num) => {
 
 }
 let result = CountDigit(-7685)
+let result2 = CountDigit(12123213)
 console.log(result)
+console.log(result2)
